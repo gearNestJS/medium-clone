2fdd86b68bc485335fdcbbe8deb2238488b4a09e
Moe Green <gearmobile@gmail.com>
2022-05-01 18:05:20 +0300

    add swagger support

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,11 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tags')
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Returns a list of tags' })
+  @ApiResponse({
+    status: 200,
+    description: 'Returns a list of tags',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error',
+  })
   findAll(): string[] {
     return this.tagService.findAll();
   }
