2428c683caf7a41b3334f010f53d38cd1bd07174
Moe Green <gearmobile@gmail.com>
2022-05-03 19:12:24 +0300

    add throw exception

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,13 +14,24 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const { email, username } = createUserDto;
+    const userByEmail: UserEntity = await this.userRepository.findOne({
+      email,
+    });
+    const userByUsername: UserEntity = await this.userRepository.findOne({
+      username,
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'User already exists',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const user: CreateUserDto = await this.userMapper.mapRequestUser(
       createUserDto,
     );
-    const savedUser: UserEntity = await this.userRepository.save(user);
-    if (!savedUser) {
-      new HttpException('error', HttpStatus.NOT_IMPLEMENTED);
-    }
-    return savedUser;
+    return await this.userRepository.save(user);
   }
 }
