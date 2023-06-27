411495c9bed9570d3fcaaff9cf4a49a9ccb6e992
Valery Semenenko <gearmobile@gmail.com>
2023-06-27 23:22:08 +0300

    install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TagModule],
+  imports: [
+    TagModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mediumclone',
+      password: 'xyzzy',
+      database: 'mediumclone',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
