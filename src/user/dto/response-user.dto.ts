a7b6f2a521c5d6ad7adcc40d1d8e82a63f3d85f5
Valery Semenenko <gearmobile@gmail.com>
2023-06-28 23:48:49 +0300

    add response user dto

---
+++ src/user/dto/response-user.dto.ts
@@ -0,0 +1,8 @@
+export class ResponseUserDto {
+  user: {
+    username: string;
+    email: string;
+    bio: string;
+    image: string;
+  };
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, ResponseUserDto } from './dto';
 
 @Controller('user')
 export class UserController {
@@ -16,7 +16,13 @@ export class UserController {
   public async createUser(
     @Body('user')
     createUserDto: CreateUserDto,
-  ): Promise<UserEntity> {
-    return await this.userService.createUser(createUserDto);
+  ): Promise<ResponseUserDto> {
+    const createUser = await this.userService.createUser(createUserDto);
+    const { id, password, ...rest } = createUser;
+    return {
+      user: {
+        ...rest,
+      },
+    };
   }
 }
